fc10c0176a0eed1951c299507c73a00195a257d1
suslecz.viktoria <suslecz.viktoria@C102-15>
2020-02-04 14:04:24 +0100

    Feltöltés függvény

--- aknakereso/Program.cs
+++ aknakereso/Program.cs
@@ -8,12 +8,43 @@ namespace aknakereso
 {
     class Program
     {
-        char [,] pálya = new char[10,10];
         static void Main(string[] args)
         {
-            Feltöltés();
+            char[,] pálya = new char[10, 10];
+
+            Feltöltés(pálya);
+            Kirajzoló();
             Console.ReadKey();
         }
-        
+
+        static void Feltöltés(char[,] pálya)
+        {
+            for (int i = 0; i < pálya.GetLength(0); i++)
+            {
+                for (int j = 0; j < pálya.GetLength(1); j++)
+                {
+                    pálya[i, j] = '_';
+                }
+            }
+
+            Random rnd = new Random();
+            int sor, oszlop;
+
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    sor = rnd.Next(10);
+                    oszlop = rnd.Next(10);
+
+                } while (pálya[sor, oszlop] == 'B');
+                pálya[sor, oszlop] = 'B';
+            }
+        }
+
+        static void Kirajzoló()
+        {
+            
+        }               
     }
 }
